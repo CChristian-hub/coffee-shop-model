58fc8d76a9f2d2ff67510f097de08b655601f666
C.Christian <christian.chang.pro@gmail.com>
2024-11-14 02:10:33 +0100

    Add/Feat: Header/navbar done, transition effect and icons in, fonts added, scss, and variables for fonts

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header class="header">
+    <a href="/">Frenchffee</a>
+    <div>
+        <a href="/">TROUVER VOTRE FRENCH <i class="fas fa-map-marker-alt"></i></a>
+        <a href="/"><i class="fas fa-bars"></i></a>
+    </div>
+</header>
